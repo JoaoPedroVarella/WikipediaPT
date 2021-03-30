081195a780591343976b6acd3902b6a5966e72db
JoaoPedroVarella <jpvarella09@gmail.com>
2021-03-30 17:33:48 -0300

    Relatórios e Runner, criada a task regressionTests para chamar o Runner

---
+++ target/relatorioSimples.html/report.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Post.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Search",
+  "description": "",
+  "id": "search",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 3761488900,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 2,
+  "name": "Search Easter egg",
+  "description": "",
+  "id": "search;search-easter-egg",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 3,
+  "name": "that you access wikipedia in portugues",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 4,
+  "name": "search for \"Ovo de Páscoa\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 5,
+  "name": "displays the expression \"Ovo de Páscoa\" in the title of the tab",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Post.thatYouAccessWikipediaInPortugues()"
+});
+formatter.result({
+  "duration": 2495772100,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Ovo de Páscoa",
+      "offset": 12
+    }
+  ],
+  "location": "Post.searchFor(String)"
+});
+formatter.result({
+  "duration": 2047944800,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Ovo de Páscoa",
+      "offset": 25
+    }
+  ],
+  "location": "Post.displaysTheExpressionInTheTitleOfTheTab(String)"
+});
+formatter.result({
+  "duration": 35446600,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 28000,
+  "status": "passed"
+});
+});
